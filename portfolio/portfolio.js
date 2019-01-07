89fd967d62c3c36f1089fab380980ccacf40ff9f
Moto42 <moto42@gmail.com>
2019-01-07 02:30:26 -0600

    Initial .js for page. The closed tag is now added to all Project_tiles upon loading the document. Even listener added to all project_tiles: they will now open/close when clicked.

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,5 +1,23 @@
 
+function closeAllTiles(tiles){
+  for(var tile = 0; tile < tiles.length; tile++){
+    tiles[tile].classList.add('closed')
+  }
+}
+
 //TODO put your DOM manipulating 'do everyting' function in here.
 document.addEventListener("DOMContentLoaded", function() {
-  // code...
+  const projectTiles = document.getElementsByClassName("project_tile");
+  closeAllTiles(projectTiles);
+  for (var num = 0; num < projectTiles.length; num++){
+    const tile = projectTiles[num];
+    tile.getElementsByClassName("project_miniview")[0].addEventListener("click",function(event){
+      if(!tile.classList.contains('closed')) tile.classList.add('closed');
+      else {
+        closeAllTiles(projectTiles);
+        tile.classList.remove('closed');
+        tile.scrollIntoView({behavior:'smooth',alignToTop:true});
+      }
+    });
+  }
 });
