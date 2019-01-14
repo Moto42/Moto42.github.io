3b335dd1d1f3695419cff4582671c39f9f64292b
Moto42 <moto42@gmail.com>
2019-01-14 17:51:53 -0600

    Updated .js for smoothscroll to contact form The old javascript could only handle one link that scrolls to the contact form. updated it to add the smoothscrolling event listener to all elements with the .contactLink class.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-window.onload = function(){
-  const contactLink = document.getElementById("contact-link");
+function addSmoothScrollToContactMe(contactLink){
   contactLink.addEventListener("click",event => {
     event.preventDefault();
-    document.getElementById("contact").scrollIntoView({behavior:'smooth'});
+    document.getElementById("contact-form").scrollIntoView({behavior:'smooth'});
   });
+}
+
+window.onload = function(){
+  const contactLinks = document.getElementsByClassName("contactLink");
+  for (let i = 0; i < contactLinks.length; i++){
+    addSmoothScrollToContactMe(contactLinks[i]);
+  }
 }
