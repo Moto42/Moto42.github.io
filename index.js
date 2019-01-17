52d6aa08c3429e50fa0155d22cc351eb5ffbb74b
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2019-01-17 01:43:08 -0600

    Spectre rebuild- Frontpage Ready (#7)
    * Complete wipe and rebuild of the frontpage.
    Wiped out all the index.html code, and rebuilt basic skeleton and 
    content.
    
    * Added Google Verification meta-tag
    
    * Linking index.js to index.html
    Renamed script.js to index.js to reflect that it is specific to that 
    file.
    Linked index.html to index.js to get the script running
    
    * Updated .js for smoothscroll to contact form
    The old javascript could only handle one link that scrolls to the 
    contact form. updated it to add the smoothscrolling event listener to 
    all elements with the .contactLink class.
    
    * Added a system fontstack to style.css, made it the default font for body
    
    * First pass of layout fixes.
    Set body max-width to 8in.
    Set herobanner's height.
    Set the .tiles to take 1fr each.
    
    * shrank #heroBanner to 30vh high
    
    * Contact form  layout first pass.
    Set content form to display grid.
    and formatted it to put the labels in left column, fields in right.
    
    * Added css for .required.
    .required items are now prefixed with an * and have the color 
    --text-important.
    
    * Fixed links to projects in Resume.html
    
    * Body is centered by default now.
    Added margin-left/right: auto to style.css
    
    * Fixe layout of contact form. kinda
    
    * A start-fron scratch complete rebuild 
    This time, instead of doing everything the hard way, we are using the 
    Spectre css framework.
    
    * Layout looking good now.
    
    * Corrected ids and classes to be targeted by index.js;
    
    * Added autofocus to the contact links.
    Whene the contact links are clicked, the #contact-name field is given 
    focus.
    
    * removed unused class from navbar
    
    * Navbar choices shrink to hamburger for small screens.
    
    * Clamped body max-width to 1100px;
    
    * centered body and linked index.html to index.css
    
    * Created OLDstyle.css and linked to current portfolio and resume .html
    
    * moved body width clamping to style.css
    
    * First pass of test navs
    
    * Added onion style header
    
    * fixed cnn and onion headers for small screens
    
    * Frontpage done

---
+++ index.js
@@ -0,0 +1,14 @@
+function addSmoothScrollToContactMe(contactLink){
+  contactLink.addEventListener("click",event => {
+    event.preventDefault();
+    document.getElementById("contact-form").scrollIntoView({behavior:'smooth'});
+    document.getElementById("contact-name").focus({preventScroll:true});
+  });
+}
+
+window.onload = function(){
+  const contactLinks = document.getElementsByClassName("contactLink");
+  for (let i = 0; i < contactLinks.length; i++){
+    addSmoothScrollToContactMe(contactLinks[i]);
+  }
+}
--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-  const contactLink = document.getElementById("contact-link");
-  contactLink.addEventListener("click",event => {
-    event.preventDefault();
-    document.getElementById("contact").scrollIntoView({behavior:'smooth'});
-  });
-}
